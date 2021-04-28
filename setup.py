cc32465768c5b403301b6dd87d6d5b7faaaf52f5
jpclipffel <jpclipffel@users.noreply.github.com>
2021-04-28 15:07:31 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+  name='m42pl-kvstores',
+  author='@jpclipffel',
+  url='https://github.com/jpclipffel/m42pl-kvstores',
+  version='1.0.0',
+  packages=['m42pl_kvstores',],
+  install_requires=[
+    'm42pl',
+    # ---
+    'aioredis==1.3.1',
+  ]
+)
