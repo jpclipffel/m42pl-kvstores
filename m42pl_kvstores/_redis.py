f80c91ad671cf44c00f4e0be678071660abae5c8
jpclipffel <jpclipffel@users.noreply.github.com>
2021-05-07 23:27:30 +0200

    Updated the `redis` KVStore
    * KVStore keys are now prefixed (new `__init__` (kw)arg => `prefix:str =
      'm42pl.'`)
    * The `redis` KVstore automatically add prefix to the keys

--- m42pl_kvstores/_redis.py
+++ m42pl_kvstores/_redis.py
@@ -12,16 +12,20 @@ class Redis(KVStore):
 
     _aliases_ = ['redis',]
 
-    def __init__(self, url: str = 'redis://127.0.0.1:6379', prefix: str = 'm42pl', *args, **kwargs):
+    def __init__(self, url: str = 'redis://127.0.0.1:6379',
+                    prefix: str = 'm42pl.', *args, **kwargs):
         """
         :param url:     Redis URL
-        :param prefix:  Redis keys prefix; Default to 'm42pl'
+        :param prefix:  Redis keys prefix; Default to 'm42pl.'
         """
         super().__init__(url, prefix, *args, **kwargs)
         self.url = url
         self.prefix = prefix
         self.collections = {}
         self.redis = None
+
+    def make_key(self, key: str):
+        return f'{self.prefix}{key}'
 
     async def __aenter__(self):
         self.redis = await aioredis.create_redis_pool(self.url)
@@ -34,9 +38,14 @@ class Redis(KVStore):
 
     async def read(self, key: str = None, default: Any = None) -> Any:
         try:
-            return msgpack.unpackb(await self.redis.get(key))
+            return msgpack.unpackb(
+                await self.redis.get(self.make_key(key))
+            )
         except Exception:
             return default
     
     async def write(self, key: str, value: Any = None) -> None:
-        await self.redis.set(key, msgpack.packb(value or ''))
+        await self.redis.set(
+            self.make_key(key),
+            msgpack.packb(value or '')
+        )
