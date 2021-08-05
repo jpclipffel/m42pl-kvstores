12c935eef949ad70615bdea16b4f699bf411657c
jpclipffel <jpclipffel@users.noreply.github.com>
2021-08-05 16:41:30 +0200

    Rewrote local KVStore
    The previous local KVStore was using a in-memory dict. It was replaced
    by a shelve which make the KVStore persistent and free of garbage
    collector problems.

--- m42pl_kvstores/local.py
+++ m42pl_kvstores/local.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+import pathlib
+import shelve
+
 from m42pl.kvstores import KVStore
 
 from typing import Any
@@ -10,17 +15,52 @@ class LocalKVStore(KVStore):
     preserve its state once destroyed.
     """
 
-    _aliases_ = ['local',]
+    _aliases_ = ['local', 'shelve']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.kvs = {}
+    def __init__(self, path: None|str = None, *args, **kwargs):
+        """
+        :param path:    KVStore shelf path
+        """
+        super().__init__(path, *args, **kwargs)
+        self.path = str(pathlib.Path(path or '~/.m42pl_kvstore').expanduser().absolute())
+        self.shelve = None
     
+    async def __aenter__(self):
+        # Try to open in read+write+create
+        try:
+            self.logger.info('attempting to open shelve')
+            self.shelve = shelve.open(self.path, flag='c')
+        # Retry to open in read only
+        except Exception:
+            try:
+                self.logger.info('attempting to open shelve in read-only mode')
+                self.shelve = self.shelve(self.path, flag='r')
+            except Exception:
+                raise
+        self.logger.info(f'shelve opened')
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        try:
+            self.logger.info('closing shelve')
+            self.shelve.close()
+            self.logger.info(f'shelve closed')
+        except Exception as error:
+            self.logger.warn(f'failed to close shelve: {str(error)}')
+            self.logger.exception(error)
+
     async def read(self, key: str, default: Any = None) -> Any:
-        return self.kvs.get(key, default)
+        try:
+            return self.shelve[key]
+        except KeyError:
+            return default
     
     async def write(self, key: str, value: Any = None) -> None:
-        self.kvs[key] = value
+        self.shelve[key] = value
 
-    async def delete(self, key: str = None, default: Any = None) -> Any:
-        return self.kvs.pop(key, default)
+    async def remove(self, key: str = None, default: Any = None) -> Any:
+        try:
+            del self.shelve[key]
+        except Exception:
+            pass
+        return default
